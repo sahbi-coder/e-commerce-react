c94986e791aac4b2dc5cce881ed96c9fb2a80179
unknown <you@example.com>
2022-06-17 19:06:29 +0200

    fixing-token-error

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
 const BASE_URL= 'http://localhost:5000/api/'
+const storage = JSON.parse(localStorage.getItem('persist:root'))
 
-const user = JSON.parse(JSON.parse( localStorage.getItem('persist:root')).user)
-const currentUser = user?user.currentUser:null
+const user = storage?storage.user:null
+const currentUser = user?JSON.parse(user).currentUser:null
+
 const token= currentUser?currentUser.token:''
 
 export const publicRequest = axios.create({
